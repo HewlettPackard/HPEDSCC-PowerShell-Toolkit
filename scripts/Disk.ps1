ecd0061cc6859aec5494be0f4eb3920b7ad16d36
Roy Atkins <atkinsroy@hotmail.com>
2023-04-30 15:20:15 +1000

    Refactored Disk.ps1

--- scripts/Disk.ps1
+++ scripts/Disk.ps1
@@ -6,28 +6,34 @@
     You must be connected to HPE GreenLake via a valid account.
 .PARAMETER SystemID
     Accepts one or more storage system IDs. Disks from all storage systems associated with the current HPE GreenLafe account
-    will be displayed if no system id is specified. 
+    will be displayed if no system id is specified.
+.PARAMETER DiskId
+    Accepts one or more disk IDs.
 .EXAMPLE
-    PS:> Get-DSCCStorageSystem -DeviceType device-type1 | Get-DSCCdisk
+    PS:> Get-DsccStorageSystem -DeviceType device-type1 | Get-Dsccdisk
 
-    This command displays the disks from all storage systems that are device type 1 (HPE Alletra 9000, HPE Primera and HPE 3PAR)
+    Display the disks from all storage systems that are device type 1 (HPE Alletra 9000, HPE Primera and HPE 3PAR)
  .EXAMPLE
-    PS:> Get-DSCCStorageSystem -DeviceType device-type2 | Get-DSCCdisk
+    PS:> Get-DsccStorageSystem -DeviceType device-type2 | Get-Dsccdisk
 
-    This command displays the disks from all storage systems that are device type 1 (HPE Alletra 6000 and 5000)
+    Display the disks from all storage systems that are device type 1 (HPE Alletra 6000 and 5000)
 .EXAMPLE
-    PS:> Get-DSCCDisk -SystemId 2M2042059T
+    PS:> Get-DsccDisk -SystemId 2M2042059T
 
-    This command displays the disks from the specified device type 1 storage system.
+    Display the disks from the specified device type 1 storage system.
 .EXAMPLE
-    PS:> Get-DSCCDisk -SystemId 000849204632ec0d70000000000000000000000001
+    PS:> Get-DsccDisk -SystemId 000849204632ec0d70000000000000000000000001
 
-    This command displays the disks from the specified device type 2 storage system.
+    Display the disks from the specified device type 2 storage system.
 .EXAMPLE
-    PS:> Get-DSCCDisk -SystemId 000849204632ec0d70000000000000000000000001 -WhatIf -Verbose
+    PS:> Get-DsccDisk -SystemId 000849204632ec0d70000000000000000000000001 -WhatIf -Verbose
 
-    This command displays information about the REST call itself rather than the data from the array(s), consisting of
+    Display information about the REST call itself rather than the data from the array(s), consisting of
     the URI, header, method and body of the call to the API. This is useful for troubleshooting.
+.EXAMPLE
+    PS:> Get-DsccDisk -DiskId ''89cc60ca6bc53785aa9ffc07f62eed7b','ec6b431eaec10bc32f1aa5aab67faa93'
+
+    Display information about the specified disk or disks. 
 .LINK
     https://github.com/HewlettPackard/HPEDSCC-PowerShell-Toolkit
 #> 
@@ -36,7 +42,9 @@ function Get-DsccDisk {
     param (
         [parameter(ValueFromPipeLineByPropertyName)]
         [alias('id')]
-        [string[]]$SystemId = ((Get-DSCCStorageSystem).Id)
+        [string[]]$SystemId = ((Get-DSCCStorageSystem).Id),
+
+        [string[]]$DiskId
     )
     begin {
         Write-Verbose 'Executing Get-DsccDisk'
@@ -51,12 +59,18 @@ function Get-DsccDisk {
                 foreach ( $ShelfId in ((Get-DSCCShelf -SystemId $ThisId).Id)) {
                     $UriAdd = "storage-systems/$DeviceType/$ThisId/enclosures/$ShelfId/enclosure-disks"
                     $RawObject = Invoke-DsccRestMethod -uriAdd $UriAdd  -Method Get -WhatIf:$WhatIfPreference
+                    if ($PSBoundParameters.ContainsKey('DiskId')) {
+                        $RawObject = $RawObject | Where-Object id -In $DiskId
+                    }
                     Invoke-RepackageObjectWithType -RawObject $RawObject -ObjectName 'Disk.Combined'
                 }
             }
             elseif ($DeviceType -eq 'device-type2') {
                 $UriAdd = "storage-systems/$DeviceType/$ThisId/disks"
                 $RawObject = Invoke-DsccRestMethod -uriadd $UriAdd -Method Get -WhatIf:$WhatIfPreference
+                if ($PSBoundParameters.ContainsKey('DiskId')) {
+                    $RawObject = $RawObject | Where-Object id -In $DiskId
+                }
                 Invoke-RepackageObjectWithType -RawObject $RawObject -ObjectName 'Disk.Combined'
             }
             else {
